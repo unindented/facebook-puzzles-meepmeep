4580d65473e6f701598108efdefeab5c2bc3fbb3
Daniel Perez Alvarez <unindented@gmail.com>
2011-06-08 13:08:33 +0200

    Added Java version

---
+++ java/meepmeep.java
@@ -0,0 +1,12 @@
+public class meepmeep
+{
+  private static void meepmeep()
+  {
+    System.out.println("Meep meep!");
+  }
+
+  public static void main(final String[] args) throws Exception
+  {
+    meepmeep();
+  }
+}
